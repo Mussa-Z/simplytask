3390f20e5f621c2dd9810da22a6a0aee8aebf96f
Abdul Zalmay <azalmay@gmail.com>
2023-07-30 19:02:57 -0400

    installed navigator libraries and dependencies

--- simplytask-app/App.js
+++ simplytask-app/App.js
@@ -1,12 +1,45 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen( { navigation } ) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button 
+        title="View details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen( { navigation }) {
+  return(
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button 
+        title="View details...again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
