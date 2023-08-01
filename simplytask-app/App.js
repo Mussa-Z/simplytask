f7da987b24c765fc55f30185692bc288e573dc4e
Abdul Zalmay <azalmay@gmail.com>
2023-08-01 19:26:43 -0400

    added all navigators - navigation shell in place

--- simplytask-app/App.js
+++ simplytask-app/App.js
@@ -6,10 +6,47 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen( { navigation } ) {
+// function GroceriesList( { navigation } ) {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Groceries List</Text>
+//       <Button 
+//         title="View details"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//     </View>
+//   );
+// }
+
+// function PartyList( { navigation } ) {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Party List</Text>
+//       <Button 
+//         title="View details"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//     </View>
+//   );
+// }
+
+// function AssignmentToDos( { navigation } ) {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Assignment To Dos</Text>
+//       <Button 
+//         title="View details"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//     </View>
+//   );
+// }
+
+function ListScreen( { route, navigation }) {
+  const listName = route.params.listName;
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text>{listName}</Text>
       <Button 
         title="View details"
         onPress={() => navigation.navigate('Details')}
@@ -30,15 +67,108 @@ function DetailsScreen( { navigation }) {
   );
 }
 
+function StatsScreen( { navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Stats Screen</Text>
+    </View>
+  );
+}
+
+function RewardsScreen( { navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Rewards Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen( { navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
+
+function ListDrawerContent({ navigation }) {
+  return (
+    <View>
+      <Button 
+        title = "Groceries List"
+        onPress={ () => {
+          navigation.navigate('List', {listName: 'Groceries List'});
+        }}
+      />
+      <Button 
+        title = "Party List"
+        onPress={ () => {
+          navigation.navigate('List', {listName: 'Party List'});
+        }}
+      />
+      <Button 
+        title = "Assignment To Dos"
+        onPress={ () => {
+          navigation.navigate('List', {listName: 'Assignment To Dos'});
+        }}
+      />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+// function ListsDrawer() {
+//   return (
+//     <Drawer.Navigator>
+//       <Drawer.Screen name="Groceries List" component={GroceriesList} />
+//       <Drawer.Screen name="Party List" component={PartyList} />
+//       <Drawer.Screen name="Assignment To Dos" component={AssignmentToDos} />
+//     </Drawer.Navigator>
+//   );
+// }
+
+function ListsDrawer() {
+  return (
+    <Drawer.Navigator
+      initialRouteName='ListScreen'
+      drawerContent={(props) => <ListDrawerContent {...props}/>}
+    >
+      <Drawer.Screen name="List" component={ListScreen} initialParams={{listName: 'No List Selected'}}/>
+    </Drawer.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function BottomTabs() {
+  return(
+    <Tab.Navigator>
+      <Tab.Screen options={{headerShown: false}} name="Lists" component={ListsDrawer} />
+      <Tab.Screen name="Stats" component={StatsScreen} />
+      <Tab.Screen name="Rewards" component={RewardsScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+
+      {/* to hide header on all screens, add to stack.navigator: screenOptions={{headerShown: false}} */}
+      {/* <Stack.Navigator> 
+        <Stack.Screen options={{headerShown: false}} name="Lists" component={ListsDrawer}/>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator> */}
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen options={{headerShown: false}} name="BottomMenuWrapper" component={BottomTabs} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
+
+
     </NavigationContainer>
   );
 }
